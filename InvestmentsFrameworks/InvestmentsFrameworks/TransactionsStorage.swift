f7ae7d2afd04651cc914edef2c0a758b02f0dc4a
Litvak Lev <sn.delui@gmail.com>
2022-09-13 16:30:04 +0500

    Remove 'TransactionsStorage' and 'TransactionStorageTests' since it doesn't contain any logic (just forwarding requests to a store). 'CoreDataStore' which conforms 'TransactionsStore' protocol will be enough

--- InvestmentsFrameworks/InvestmentsFrameworks/TransactionsStorage.swift
+++ InvestmentsFrameworks/InvestmentsFrameworks/TransactionsStorage.swift
@@ -7,26 +7,6 @@
 
 import Foundation
 
-public final class TransactionsStorage {
-    let store: TransactionsStore
-    
-    public enum RetrivalError: Error {
-        case failed
-    }
-    
-    public init(store: TransactionsStore) {
-        self.store = store
-    }
-    
-    public func retrieve() throws -> [Transaction] {
-        do {
-            return try store.retrieve()
-        } catch {
-            throw RetrivalError.failed
-        }
-    }
-}
-
 public protocol TransactionsStore {
     func retrieve() throws -> [Transaction]
     func save(transaction: Transaction) throws
