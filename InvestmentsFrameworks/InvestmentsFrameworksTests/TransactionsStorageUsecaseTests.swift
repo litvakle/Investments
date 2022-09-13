45b033809b35902b888e6f490ec48e7ce385e641
Litvak Lev <sn.delui@gmail.com>
2022-09-13 11:00:24 +0500

    Create a separate target for framework tests

---
+++ InvestmentsFrameworks/InvestmentsFrameworksTests/TransactionsStorageUsecaseTests.swift
@@ -0,0 +1,14 @@
+//
+//  InvestmentsFrameworksTests.swift
+//  InvestmentsFrameworksTests
+//
+//  Created by Lev Litvak on 13.09.2022.
+//
+
+import XCTest
+
+class TransactionsStorageUsecaseTests: XCTestCase {
+    func test() {
+        
+    }
+}
