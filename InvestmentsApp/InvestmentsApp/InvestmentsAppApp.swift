1509533d4fd5c09d109673d1c63a142042bef682
Litvak Lev <sn.delui@gmail.com>
2022-09-15 09:15:13 +0500

    Add 'InvestmentsApp' and create a workspace for frameworks and app

---
+++ InvestmentsApp/InvestmentsApp/InvestmentsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvestmentsAppApp.swift
+//  InvestmentsApp
+//
+//  Created by Lev Litvak on 15.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct InvestmentsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
