393dc0410d34dc1c9e7e751c6c4eb757899c1d13
Litvak Lev <sn.delui@gmail.com>
2022-09-15 15:42:05 +0500

    'TransactionViewModel' edit requisites affects on correction checking

--- InvestmentsFrameworks/InvestmentsFrameworksTests/Transactions/Presentation/TransactionViewModelTests.swift
+++ InvestmentsFrameworks/InvestmentsFrameworksTests/Transactions/Presentation/TransactionViewModelTests.swift
@@ -7,15 +7,23 @@
 
 import XCTest
 import InvestmentsFrameworks
+import Combine
 
 class TransactionViewModel: ObservableObject {
     @Published private(set) var id: UUID
-    @Published private(set) var date: Date
-    @Published private(set) var type: TransactionType
-    @Published private(set) var ticket: String
-    @Published private(set) var quantity: Double
-    @Published private(set) var price: Double
-    @Published private(set) var sum: Double
+    @Published var date: Date
+    @Published var type: TransactionType
+    @Published var ticket: String
+    @Published var quantity: Double
+    @Published var price: Double
+    @Published var sum: Double
+    
+    @Published private(set) var ticketErrorMessage: String?
+    @Published private(set) var quantityErrorMessage: String?
+    @Published private(set) var priceErrorMessage: String?
+    @Published private(set) var sumErrorMessage: String?
+    
+    private var cancellables = Set<AnyCancellable>()
     
     init(transaction: Transaction) {
         self.id = transaction.id
@@ -25,6 +33,38 @@ class TransactionViewModel: ObservableObject {
         self.quantity = transaction.quantity
         self.price = transaction.price
         self.sum = transaction.sum
+        
+        setupSubsriptions()
+    }
+    
+    private func setupSubsriptions() {
+        $ticket
+            .map { $0.count < 3 ? "Ticket should contain at least 3 symbols" : nil }
+            .sink { [weak self] message in
+                self?.ticketErrorMessage = message
+            }
+            .store(in: &cancellables)
+        
+        $quantity
+            .map { $0 <= 0 ? "Quantity should be below than zero" : nil }
+            .sink { [weak self] message in
+                self?.quantityErrorMessage = message
+            }
+            .store(in: &cancellables)
+        
+        $price
+            .map { $0 <= 0 ? "Price should be below than zero" : nil }
+            .sink { [weak self] message in
+                self?.priceErrorMessage = message
+            }
+            .store(in: &cancellables)
+        
+        $sum
+            .map { $0 <= 0 ? "Sum should be below than zero" : nil }
+            .sink { [weak self] message in
+                self?.sumErrorMessage = message
+            }
+            .store(in: &cancellables)
     }
 }
 
@@ -40,6 +80,32 @@ class TransactionViewModelTests: XCTestCase {
         XCTAssertEqual(sut.quantity, transaction.quantity)
         XCTAssertEqual(sut.price, transaction.price)
         XCTAssertEqual(sut.sum, transaction.sum)
+    }
+    
+    func test_editRequisites_affectsOnCorrectionChecking() {
+        let transaction = makeTransaction()
+        let sut = makeSUT(transaction: transaction)
+        
+        sut.ticket = ""
+        XCTAssertNotNil(sut.ticketErrorMessage)
+        
+        sut.quantity = 0
+        XCTAssertNotNil(sut.quantityErrorMessage)
+        
+        sut.quantity = -1
+        XCTAssertNotNil(sut.quantityErrorMessage)
+        
+        sut.price = 0
+        XCTAssertNotNil(sut.priceErrorMessage)
+        
+        sut.price = -1
+        XCTAssertNotNil(sut.priceErrorMessage)
+        
+        sut.sum = 0
+        XCTAssertNotNil(sut.sumErrorMessage)
+        
+        sut.sum = -1
+        XCTAssertNotNil(sut.sumErrorMessage)
     }
     
     // MARK: - Helpers
