e2781d58e4e8755af77f6b6bd25e32b90e2c8e57
Litvak Lev <sn.delui@gmail.com>
2022-09-20 18:22:28 +0500

    'TransactionsView' handles transaction select

--- InvestmentsApp/InvestmentsAppTests/TransactionsViewTests.swift
+++ InvestmentsApp/InvestmentsAppTests/TransactionsViewTests.swift
@@ -14,16 +14,43 @@ extension TransactionsView: Inspectable {}
 
 class TransactionsViewTests: XCTestCase {
     func test_transactionView_rendersTransactions() throws {
-        let store = InMemoryTransactionsStore()
-        let viewModel = TransactionsViewModel(store: store)
-        let sut = TransactionsView(viewModel: viewModel, onTransactionSelect: { _ in }, onTransactionDelete: { _ in })
+        let (sut, viewModel, store) = makeSUT()
         
         viewModel.retrieve()
         
         XCTAssertEqual(try sut.inspect().list().forEach(0).count, store.transactions.count)
     }
     
+    func test_transactionView_handlesTransactionSelect() throws {
+        var selectedTransactions = [Transaction?]()
+        let (sut, viewModel, store) = makeSUT(onSelect: { selectedTransaction in
+            selectedTransactions.append(selectedTransaction)
+        })
+        
+        viewModel.retrieve()
+        try sut.inspect().list().forEach(0).button(0).tap()
+        try sut.inspect().list().forEach(0).button(0).tap()
+        try sut.inspect().list().forEach(0).button(1).tap()
+        
+        XCTAssertEqual(selectedTransactions, [store.transactions[0], store.transactions[0], store.transactions[1]])
+    }
+    
     // MARK: - Helpers
+    
+    private func makeSUT(
+        onSelect: @escaping (Transaction?) -> Void = { _ in },
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> (sut: TransactionsView, viewModel: TransactionsViewModel, store: InMemoryTransactionsStore) {
+        let store = InMemoryTransactionsStore()
+        let viewModel = TransactionsViewModel(store: store)
+        let sut = TransactionsView(
+            viewModel: viewModel,
+            onTransactionSelect: onSelect,
+            onTransactionDelete: { _ in })
+        
+        return (sut, viewModel, store)
+    }
     
     class InMemoryTransactionsStore: TransactionsStore {
         var transactions = [
