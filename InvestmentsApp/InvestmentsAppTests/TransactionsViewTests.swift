d4747411e57d9709bba9e4d4c86f928ec550fd8f
Litvak Lev <sn.delui@gmail.com>
2022-09-20 17:28:30 +0500

    Add 0.9.1 version of 'ViewInspector' since 0.9.2 requires XCode14

---
+++ InvestmentsApp/InvestmentsAppTests/TransactionsViewTests.swift
@@ -0,0 +1,14 @@
+//
+//  TransactionsViewTests.swift
+//  InvestmentsAppTests
+//
+//  Created by Lev Litvak on 20.09.2022.
+//
+
+import XCTest
+
+class TransactionsViewTests: XCTestCase {
+    func test() {
+        
+    }
+}
