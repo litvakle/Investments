b8f032d69eaa451c407e64b2735d8b4fb3c90503
Litvak Lev <sn.delui@gmail.com>
2022-09-26 12:45:45 +0500

    Extract 'anyNSError' in a separate test helpers file

---
+++ InvestmentsApp/InvestmentsAppTests/SharedTestHelpers.swift
@@ -0,0 +1,12 @@
+//
+//  SharedTestHelpers.swift
+//  InvestmentsAppTests
+//
+//  Created by Lev Litvak on 26.09.2022.
+//
+
+import Foundation
+
+func anyNSError() -> NSError {
+    NSError(domain: "Any error", code: 0)
+}
