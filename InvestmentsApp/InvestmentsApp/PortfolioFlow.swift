01a998032bc82ebe85877a61d9447956dc7f85aa
Litvak Lev <sn.delui@gmail.com>
2022-09-27 08:52:56 +0500

    'TransactionsViewModel.saveTransactions' leads to update 'PortfolioViewModel.items' via 'PortfolioFlow'

---
+++ InvestmentsApp/InvestmentsApp/PortfolioFlow.swift
@@ -0,0 +1,25 @@
+//
+//  PortfolioFlow.swift
+//  InvestmentsApp
+//
+//  Created by Lev Litvak on 27.09.2022.
+//
+
+import Foundation
+import Combine
+import InvestmentsFrameworks
+
+class PortfolioFlow: ObservableObject {
+    var cancellables = Set<AnyCancellable>()
+    
+    init() {}
+    
+    func setupSubscriptions(portfolioViewModel: PortfolioViewModel, transactionsViewModel: TransactionsViewModel) {
+        transactionsViewModel.$transactions
+            .removeDuplicates()
+            .sink { transactions in
+                portfolioViewModel.createItems(for: transactions)
+            }
+            .store(in: &cancellables)
+    }
+}
---
+++ InvestmentsApp/InvestmentsAppTests/Portfolio/PortfolioFlowTests.swift
@@ -0,0 +1,44 @@
+//
+//  PortfolioFlowTests.swift
+//  InvestmentsAppTests
+//
+//  Created by Lev Litvak on 27.09.2022.
+//
+
+import XCTest
+import InvestmentsFrameworks
+@testable import InvestmentsApp
+
+class PortfolioFlowTests: XCTestCase {
+    func test_saveTransaction_leadsToUpdatePortfolio() {
+        let (sut, transactionsViewModel, portfolioViewModel) = makeSUT(transactions: [])
+        
+        sut.setupSubscriptions(portfolioViewModel: portfolioViewModel, transactionsViewModel: transactionsViewModel)
+        
+        XCTAssertEqual(portfolioViewModel.items, [])
+        makePortfolioTransactions().forEach { transactionsViewModel.save($0) }
+        XCTAssertEqual(portfolioViewModel.items, makePortfolioItems())
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(
+        transactions: [Transaction],
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> (PortfolioFlow, TransactionsViewModel, PortfolioViewModel) {
+        let store = InMemoryTransactionsStore()
+        store.transactions = transactions
+        let transactionsViewModel = TransactionsViewModel(store: store)
+        transactionsViewModel.retrieve()
+        let portfolioViewModel = PortfolioViewModel()
+        let sut = PortfolioFlow()
+        
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(transactionsViewModel, file: file, line: line)
+        trackForMemoryLeaks(portfolioViewModel, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return (sut, transactionsViewModel, portfolioViewModel)
+    }
+}
