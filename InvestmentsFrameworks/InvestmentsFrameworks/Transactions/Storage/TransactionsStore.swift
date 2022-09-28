a3cb7a792d57989d25e4ffb40496fb1e3c5c529d
Litvak Lev <sn.delui@gmail.com>
2022-09-28 11:06:24 +0500

    Remove unnecessary import of 'CoreData'

--- InvestmentsFrameworks/InvestmentsFrameworks/Transactions/Storage/TransactionsStore.swift
+++ InvestmentsFrameworks/InvestmentsFrameworks/Transactions/Storage/TransactionsStore.swift
@@ -5,8 +5,6 @@
 //  Created by Lev Litvak on 13.09.2022.
 //
 
-import CoreData
-
 public protocol TransactionsStore {
     func retrieve() throws -> [Transaction]
     func save(_ transaction: Transaction) throws
