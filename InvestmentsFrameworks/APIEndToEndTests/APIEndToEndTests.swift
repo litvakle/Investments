d55d8400661401bc94b2813ec876d46021a9cca7
Litvak Lev <sn.delui@gmail.com>
2022-10-13 16:55:16 +0500

    End to end server PUT transactions result delivers successfully

--- InvestmentsFrameworks/APIEndToEndTests/APIEndToEndTests.swift
+++ InvestmentsFrameworks/APIEndToEndTests/APIEndToEndTests.swift
@@ -35,6 +35,24 @@ class APIEndToEndTests: XCTestCase {
         }
     }
     
+    func test_endToEndServerPUTTransactionsResult_deliversSuccessfully() {
+        let transactions = [
+            Transaction(id: UUID(), date: Date(timeIntervalSince1970: 1598627222), ticket: "AAA", type: .buy, quantity: 10, price: 2, sum: 20),
+            Transaction(id: UUID(), date: Date(timeIntervalSince1970: 1577881882), ticket: "BBB", type: .sell, quantity: 5, price: 20, sum: 100)
+        ]
+        
+        switch putResult(for: transactions, url: transactionsTestServerURL, encoder: TransactionsMapper.map,  decoder: TransactionsMapper.map) {
+        case let .success(uploadedTransactions)?:
+            XCTAssertEqual(uploadedTransactions, transactions)
+            
+        case let .failure(error)?:
+            XCTFail("Expected successful result, got \(error) instead")
+            
+        default:
+            XCTFail("Expected successful result, got no result instead")
+        }
+    }
+    
     // MARK: - Helpers
     
     private func getResult<T>(
@@ -62,6 +80,34 @@ class APIEndToEndTests: XCTestCase {
         return receivedResult
     }
     
+    private func putResult<T>(
+        file: StaticString = #filePath,
+        line: UInt = #line,
+        for transactions: [Transaction],
+        url: URL,
+        encoder: @escaping ([Transaction]) -> Data?,
+        decoder: @escaping (Data, HTTPURLResponse) throws -> T
+    ) -> Swift.Result<T, Error>? {
+        let client = ephemeralClient()
+        let data = encoder(transactions)!
+        let exp = expectation(description: "Wait for load completion")
+        
+        var receivedResult: Swift.Result<T, Error>?
+        client.put(data, to: url) { result in
+            receivedResult = result.flatMap { (data, response) in
+                do {
+                    return .success(try decoder(data, response))
+                } catch {
+                    return .failure(error)
+                }
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 15.0)
+        
+        return receivedResult
+    }
+    
     private func ephemeralClient(file: StaticString = #filePath, line: UInt = #line) -> HTTPClient {
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         trackForMemoryLeaks(client, file: file, line: line)
@@ -73,6 +119,6 @@ class APIEndToEndTests: XCTestCase {
     }
     
     private var transactionsTestServerURL: URL {
-        return URL(string: "https://investments-3b67e-default-rtdb.firebaseio.com/transactions.json")!
+        return URL(string: "https://investments-3b67e-default-rtdb.firebaseio.com/transactionsTest.json")!
     }
 }
