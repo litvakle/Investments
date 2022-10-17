98a294127c28e54f765eef7207a01121dbe9ba37
Litvak Lev <sn.delui@gmail.com>
2022-10-17 18:27:29 +0500

    Replace 'TransactionsStore' with three protocols according to ISP principle

--- InvestmentsFrameworks/InvestmentsFrameworks/Transactions/Storage/TransactionsStore.swift
+++ InvestmentsFrameworks/InvestmentsFrameworks/Transactions/Storage/TransactionsStore.swift
@@ -5,8 +5,16 @@
 //  Created by Lev Litvak on 13.09.2022.
 //
 
-public protocol TransactionsStore {
+public typealias TransactionsStore = TransactionsRetriever & TransactionsSaver & TransactionsDeleter
+
+public protocol TransactionsRetriever {
     func retrieve() throws -> [Transaction]
+}
+
+public protocol TransactionsSaver {
     func save(_ transaction: Transaction) throws
+}
+
+public protocol TransactionsDeleter {
     func delete(_ transaction: Transaction) throws
 }
